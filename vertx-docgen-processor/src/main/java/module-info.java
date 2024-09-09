43d6fcbcdd7b2f0cca8ff159a373651b9dfc946b
Julien Viet <julien@julienviet.com>
2024-09-09 13:54:22 +0200

    Make annotation processor discoverable

--- vertx-docgen-processor/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-import javax.annotation.processing.Processor;
-
-module io.vertx.docgen.processor {
-  requires jdk.compiler;
-  requires io.vertx.docgen;
-  exports io.vertx.docgen.processor;
-  // provides Processor with JavaDocGenProcessor;
-}
